69163fa91479520d38788f1fe32a7bb98f033b2e
Johnny <miyavi781221@gmail.com>
2023-11-29 13:22:17 +0800

    add shopping.vue

--- src/components/shopping.vue
+++ src/components/shopping.vue
@@ -1,22 +1,23 @@
-<script>
-import {ref} from 'vue'
-export default{
-    setup(){
-        return {
-            'test': "test shopping",
-            testNum: ref(1)
-        }
-    },
-    methods:{
-        'testFunc':function(){
-            return 'testFunc'
-        }        
-    },
-    components:{
-        'testComp':{ template:'<div>test Component</div>'}
-    }
-}
-</script>
 <template>
-    <testComp></testComp>
+  <testComp></testComp>
 </template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    return {
+      test: "test shopping",
+      testNum: ref(1),
+    };
+  },
+  methods: {
+    testFunc: function () {
+      return "testFunc";
+    },
+  },
+  components: {
+    testComp: { template: "<div>test Component</div>" },
+  },
+};
+</script>

c1b0de6d400443b2d174352f757569b915a50b90
YIIIIIACH <1997marc212@gmail.com>
2023-12-11 11:37:51 +0800

    temp add

--- src/views/bookingSystem/ticketOrderDetail.vue
+++ src/views/bookingSystem/ticketOrderDetail.vue
@@ -8,6 +8,7 @@ const ststName = ref('')
 const edstName = ref('')
 const bookingInfo = reactive([])
 const props = defineProps(['tckodid'])
+const ticketQRcodeSrc = ref('')
 onBeforeMount(()=>{
     // check is login
     httpClient.post('/verifyLoginToken',{},{withCredentials:true}).then((res)=>{
@@ -42,6 +43,13 @@ onBeforeMount(()=>{
         }
     })
 })
+function createTicketQR(){
+    httpClient.get('https://chart.googleapis.com/chart?cht=qr&chl=https://google.com&chs=150x150',{withCredentials:false})
+    .then(res=>{
+        // console.log(res.data)
+        ticketQRcodeSrc.value = res.data;
+    });
+}
 </script>
 <template>
     <div class="container">
@@ -52,11 +60,34 @@ onBeforeMount(()=>{
         <div v-for="b of bookingInfo" class="card-body booking-box">
             <h5 class="card-title">{{ b.ticketDiscount.ticketDiscountName }}</h5><!--{{ seatList[i].carriage }}-->
             <p class="card-text">第{{ b.seat.carriage }}車廂 {{ b.seat.seatCode }} {{ b.seat.seatDescirption }} {{ b.ticketDiscount.ticketDiscountName }}</p>
-            <a href="#" v-if="b.ticketQRcode==null" class="btn btn-primary">生成車票QRcode</a>
+            <a href="#" v-if="b.ticketQRcode==null" @click="createTicketQR" class="btn btn-primary">生成車票QRcode</a>
             <a href="#" v-else class="btn btn-primary">查看車票QRcode</a>
         </div>
         </div>
     </div>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img :src="ticketQRcodeSrc">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 <style>
 .schedule-box{
